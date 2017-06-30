33a87f64a23626a993df8da8f29125dccf88dd13
Sawyer Mifsud <wingren013@gmail.com>
2017-06-30 13:20:01 -0700

    variance

--- standardscale.c
+++ standardscale.c
@@ -1,6 +1,7 @@
 //mean_i = 1/i * (sum(1, x, n))
 
-void	assign_means(double **meanset, size_t k, size_t datasize, double *data)
+//assign_means(&empty, k, &(data[floor]), ceiling)
+void	assign_means(double **meanset, size_t k, double *data, size_t datasize)
 {
 	size_t	i = 0;
 	
@@ -11,5 +12,16 @@ void	assign_means(double **meanset, size_t k, size_t datasize, double *data)
 		*meanset[i] = data[rand() % datasize];
 		i++;
 	}
+} //this goes somwehere else techinacally
+
+double incremental_mean(double mean, double, prevmean, double newval, size_t count)
+{
+	mean = prevmean + (newval - prevmean)/count;
 }
 
+void	incremental_variance()
+{
+	variance = (n - 2)prev + (n -1)(incremental_mean(prev) - incremental_mean(now))^2;
+	varaince += (current_element - incremental_mean(now))^2;
+	variance /= n - 1;
+}
