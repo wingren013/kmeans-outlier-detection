083a2b28f562879dd1db97660d932ade540fc043
Sawyer Mifsud <wingren013@gmail.com>
2017-06-30 18:14:09 -0700

    header fixed

--- kmeans.h
+++ kmeans.h
@@ -1,6 +1,8 @@
 #ifndef KMEANS_H
 # define KMEANS_H
 
+#include <stdlib.h>
+
 #define ABS(X) ((X)<0 ? -(X) : (X))
 
 typedef struct	centroid_s
@@ -11,5 +13,10 @@ typedef struct	centroid_s
 }				centorid_t;
 
 extern centroid_t	*centroids;
+
+void	update(double *data, size_t size, size_t k);
+double	centroid_dist(centroid_t centroid);
+double	find_dist(double *data, size_t size);
+size_t	find_closest_mean(double *data, size_t size, size_t k);
 
 #endif
